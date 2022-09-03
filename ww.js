ae7649bc2b01c4fc78c05d086c6af983c4acded1
sakuraiike <sakurai1003@live.cn>
2022-09-03 14:38:05 +0800

    ce....sce

--- ww.js
+++ ww.js
@@ -3,7 +3,7 @@ exports.showme=function(){
 }
 exports.year=3333;
 exports.person={
-    name:"lily",
+    name:"lily1",
     sayhello:function(){
         console.log("hello:i am ",this.name);
     }
